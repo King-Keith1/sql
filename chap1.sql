c9f2be1aec97ca18bfb474ed6ce2a4f672ab449d
King-Keith1 <keith4321ben@gmail.com>
2025-07-24 09:48:20 +0200

    commit

--- chap1.sql
+++ chap1.sql
@@ -1,16 +1,3 @@
---------------------------------------------------------------
--- Practical SQL: A Beginner's Guide to Storytelling with Data
--- by Anthony DeBarros
-
--- Chapter 1 Code Examples
---------------------------------------------------------------
-
--- Listing 1-1: Creating a database named analysis
-
-CREATE DATABASE analysis;
-
--- Listing 1-2: Creating a table named teachers with six columns
-
 CREATE TABLE teachers (
     id bigserial,
     first_name varchar(25),
@@ -20,11 +7,6 @@ CREATE TABLE teachers (
     salary numeric
 );
 
--- This command will remove (drop) the table.
--- DROP TABLE teachers;
-
--- Listing 1-3 Inserting data into the teachers table
-
 INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
 VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
        ('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-05-22', 65000),
@@ -32,3 +14,4 @@ VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
        ('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200),
        ('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
        ('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
+
