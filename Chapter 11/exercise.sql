a130b44acbcbd31babf8db8db3df6b7f77c9baca
King-Keith1 <keith4321ben@gmail.com>
2025-08-01 15:28:03 +0200

    commit

--- Chapter 11/exercise.sql
+++ Chapter 11/exercise.sql
@@ -46,9 +46,50 @@ COPY (
     JOIN department d ON e.department_id = d.department_id
 ) TO 'C:\Bootcamp2025\SQL\sql\Chapter 11\joined_employees_departments.csv' CSV HEADER;
 
+ALTER TABLE employees
+ADD COLUMN score INTEGER;
 
+UPDATE employees SET score = 88 WHERE full_name = 'Alexander Badenhorst';
+UPDATE employees SET score = 91 WHERE full_name = 'Cadee Rousseau';
+UPDATE employees SET score = 85 WHERE full_name = 'David Koen';
+UPDATE employees SET score = 76 WHERE full_name = 'Ethan Hurwitz';
+UPDATE employees SET score = 94 WHERE full_name = 'Lindokuhle Yende';
+UPDATE employees SET score = 89 WHERE full_name = 'Marvelous Kahundahunda';
+UPDATE employees SET score = 92 WHERE full_name = 'Pierre Kahundahunda';
+UPDATE employees SET score = 74 WHERE full_name = 'Ronny Mputla';
+UPDATE employees SET score = 95 WHERE full_name = 'Sibusiso Makhiwane';
+UPDATE employees SET score = 80 WHERE full_name = 'Tom Vuma';
+UPDATE employees SET score = 87 WHERE full_name = 'Ulrich Snyman';
+UPDATE employees SET score = 65 WHERE full_name = 'Courtney Cook';
 
+ALTER TABLE employees
+ADD COLUMN low_scores INTEGER;
 
+UPDATE employees SET low_scores = 49 WHERE full_name = 'Alexander Badenhorst';
+UPDATE employees SET low_scores = 48 WHERE full_name = 'Cadee Rousseau';
+UPDATE employees SET low_scores = 0 WHERE full_name = 'David Koen';
+UPDATE employees SET low_scores = 36 WHERE full_name = 'Ethan Hurwitz';
+UPDATE employees SET low_scores = 60 WHERE full_name = 'Lindokuhle Yende';
+UPDATE employees SET low_scores = 20 WHERE full_name = 'Marvelous Kahundahunda';
+UPDATE employees SET low_scores = 44 WHERE full_name = 'Pierre Kahundahunda';
+UPDATE employees SET low_scores = 15 WHERE full_name = 'Ronny Mputla';
+UPDATE employees SET low_scores = 25 WHERE full_name = 'Sibusiso Makhiwane';
+UPDATE employees SET low_scores = 21 WHERE full_name = 'Tom Vuma';
+UPDATE employees SET low_scores = 45 WHERE full_name = 'Ulrich Snyman';
+UPDATE employees SET low_scores = 36 WHERE full_name = 'Courtney Cook';
+
+SELECT full_name, score, low_scores
+FROM employees
+WHERE (score > 50) AND (low_scores < 20);
+
+SELECT 
+  full_name,
+  score,
+  low_scores,
+  ROUND((score - (SELECT AVG(score) FROM employees)), 1) AS difference_from_avg
+FROM employees;
+
+SELECT * FROM employees
 
 
 
--- Chapter 12/chap12.sql
+++ Chapter 12/chap12.sql
@@ -51,3 +51,66 @@ FROM employeez
 WHERE emp_id IN (
     SELECT id
     FROM retirees);
+
+CREATE TABLE roles (
+    roloy_id SERIAL PRIMARY KEY,
+    role_name VARCHAR(100) UNIQUE,
+    job_description VARCHAR(100),
+    base_salary INTEGER
+);
+
+CREATE TABLE employeez (
+    emp_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    birth_date DATE,
+    num_sales INTEGER,
+    roloy_id INTEGER REFERENCES roles(roloy_id)
+);
+
+INSERT INTO roles (role_name, job_description, base_salary) 
+VALUES
+    ('Graphic Designer', 'Helps with video editing, photo editing and market advertisement designs', 35000),
+    ('Videographer', 'Helps with all digital media productions', 18000),
+    ('Social Marketer', 'Helps with social media strategies and social data', 26000),
+    ('Sales Rep', 'Helps promote and sign clients', 20000);
+
+INSERT INTO employeez (name, birth_date, num_sales, roloy_id) 
+VALUES
+    ('Aragorn', '1994-08-24', 22, 4),
+    ('Gandalf', '1982-05-11', 0, 1),
+    ('Frodo', '1999-01-18', 0, 2),
+    ('Legolas', '1998-04-22', 0, 3),
+    ('Gimli', '2000-11-08', 10, 4),
+    ('Samwise', '2001-01-01', 9, 4),
+    ('Pippin', '1999-09-26', 18, 4),
+    ('Merry', '2005-08-07', 0, 3);
+
+ALTER TABLE employeez ADD COLUMN salary INTEGER;
+
+UPDATE employeez SET salary = 22400 WHERE name = 'Aragorn';
+UPDATE employeez SET salary = 35000 WHERE name = 'Gandalf';
+UPDATE employeez SET salary = 18000 WHERE name = 'Frodo';
+UPDATE employeez SET salary = 26000 WHERE name = 'Legolas';
+UPDATE employeez SET salary = 20000 WHERE name = 'Gimli';
+UPDATE employeez SET salary = 20000 WHERE name = 'Samwise';
+UPDATE employeez SET salary = 21600 WHERE name = 'Pippin';
+UPDATE employeez SET salary = 26000 WHERE name = 'Merry';
+
+COPY (
+  SELECT 
+      e.name,
+      e.birth_date,
+      e.num_sales,
+      e.salary,
+      r.role_name,
+      r.job_description,
+      r.base_salary
+  FROM employeez e
+  JOIN roles r ON e.roloy_id = r.roloy_id
+) TO 'C:\Bootcamp2025\SQL\sql\Chapter 12\employee_report.csv' WITH CSV HEADER;
+
+SELECT name, birth_date
+FROM employeez
+WHERE AGE('2025-08-01', birth_date) > INTERVAL '27 years';
+
+
